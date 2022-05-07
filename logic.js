7ea08bff421b457e721b8c66e3bcb4a85bcbb5a8
jrobertunder <88185044+jrobertunder@users.noreply.github.com>
2022-05-07 11:42:23 -0700

    branching update

--- logic.js
+++ logic.js
@@ -13,6 +13,11 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
-
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.circle([34.0522, -118.2437], {
+  radius: 300,
+  color: 'yellow',
+  fillcolor: 'ffffa1'
+}).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
