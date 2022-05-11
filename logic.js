b4ae5cd9b55fd20fa4f08baaf38b9ba673edc76a
jrobertunder <88185044+jrobertunder@users.noreply.github.com>
2022-05-10 17:50:09 -0700

    update 5

--- logic.js
+++ logic.js
@@ -5,25 +5,25 @@ console.log("working");
 // let map = L.map('mapid').setView([30, 30], 2);
 // We create the tile layer that will be the background of our map.
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-  });
-  
-  let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-  });
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
 
-  // We create the dark view tile layer that will be an option for our map.
+let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      accessToken: API_KEY
-  });
-  
-  // Create a base layer that holds both maps.
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
 let baseMaps = {
   Light: light,
   Street: street,
@@ -40,24 +40,32 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-  // Accessing the airport GeoJSON URL
-  let airportData = "https://raw.githubusercontent.com/jrobertunder/Mapping_Earthquakes/main/majorAirports.json";
-
-  // Grabbing our GeoJSON data.
-  d3.json(airportData).then(function(data) {
-    console.log(data);
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/jrobertunder/Mapping_Earthquakes/main/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/jrobertunder/Mapping_Earthquakes/main/torontoRoutes.json";
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function (data) {
+  console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
-  });
-  // Get data from cities.js
-  let cityData = cities;
-  
-  // Loop through the cities array and create one marker for each city.
-  cityData.forEach(function(city) {
+});
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function (data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function (city) {
   console.log(city)
   L.marker(city.location)
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
-.addTo(map);
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(map);
 });
 // Coordinates for each point to be used in the line.
 //   let line = [
