88e2e417f9cf3d5a3ede37c199590db34d8997ec
jrobertunder <88185044+jrobertunder@users.noreply.github.com>
2022-05-10 17:35:15 -0700

    update 3

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 // Create the map object with a center and zoom level.
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,9 +10,32 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
   });
   
+  // We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: API_KEY
+  });
+  
+  // Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
   // Accessing the airport GeoJSON URL
   let airportData = "https://raw.githubusercontent.com/jrobertunder/Mapping_Earthquakes/main/majorAirports.json";
-  
+
   // Grabbing our GeoJSON data.
   d3.json(airportData).then(function(data) {
     console.log(data);
@@ -30,16 +53,16 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 .addTo(map);
 });
 // Coordinates for each point to be used in the line.
-  let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-];
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+//   let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+// ];
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
 
 // Add GeoJSON data.
 // let sanFranAirport =
@@ -61,4 +84,4 @@ L.polyline(line, {
 //             "coordinates":[-122.375,37.61899948120117]}}
 // ]};
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
